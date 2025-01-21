2742a418645e37bf6cebba16ffcb4a32666f0c76
urysov.k <konstantin.urysov@gmail.com>
2025-01-22 01:49:59 +0400

    bugfixing

--- internal/server/service.go
+++ internal/server/service.go
@@ -15,6 +15,7 @@ const (
 	getCommand  = "GET"
 	delCommand  = "DEL"
 	helpCommand = "help"
+	exitCommand = "exit"
 	guide       = "query = set_command | get_command | del_command \n set_command = \"SET\" argument argument \n get_command = \"GET\" argument \n del_command = \"DEL\" argument \n argument    = punctuation | letter | digit { punctuation | letter | digit } \n punctuation = \"*\" | \"/\" | \"_\" | ... \n letter      = \"a\" | ... | \"z\" | \"A\" | ... | \"Z\" \n digit       = \"0\" | ... | \"9\" "
 )
 
@@ -40,33 +41,35 @@ func (s *Server) Execute() error {
 			}
 			command, args, err := s.parser.Parse(line)
 			if err != nil {
-				return err
+				s.logger.Error(err)
 			}
 
 			switch command {
 			case setCommand:
 				if len(args) < 2 {
-					fmt.Println("failed to set: not enough arguments")
+					s.logger.Error("failed to set: not enough arguments")
 				} else {
 					s.engine.Set(args[0], args[1])
 				}
 			case getCommand:
 				if len(args) < 1 {
-					fmt.Println("failed to set: not enough arguments")
+					s.logger.Error("failed to set: not enough arguments")
 				} else {
-					fmt.Printf("%s\n", args[0])
+					fmt.Printf("%s\n", s.engine.Get(args[0]))
 				}
 			case delCommand:
 				if len(args) < 1 {
-					fmt.Println("failed to set: not enough arguments")
+					s.logger.Error("failed to delete: not enough arguments")
 				} else {
 					s.engine.Delete(args[0])
 					fmt.Printf("%s removed\n", args[0])
 				}
 			case helpCommand:
 				fmt.Println(guide)
+			case exitCommand:
+				return nil
 			default:
-				fmt.Println("wrong command, please use help command")
+				s.logger.Error("wrong command, please use help command")
 			}
 		}
 	}
